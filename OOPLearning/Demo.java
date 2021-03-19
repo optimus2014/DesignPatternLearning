e5534052802ac08e7957c25d7fed6f97acf70484
optimus2014 <forever.yjr@163.com>
2021-03-19 17:55:43 +0800

    初始化《设计模式学习》工程

---
+++ OOPLearning/Demo.java
@@ -0,0 +1,7 @@
+package OOPLearning;
+
+/**
+ * 设计模式工程，第一个Demo，练习面向对象相关内容
+ */
+public class Demo {
+}
---
+++ com/learn/DesignPatterns/main.java
@@ -0,0 +1,13 @@
+package com.learn.DesignPatterns;
+
+/***
+ * 设计模式：
+ * 常见原则：KISS，SOLID等
+ * 共23种设计模式，常用10+；
+ * 创建类：单例模式
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello,设计模式.");
+    }
+}
