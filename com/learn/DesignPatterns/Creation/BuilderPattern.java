6a5fe7cc3e2329b9cd8ab287233a8b4d9d618f20
Yanru <forever.yjr@163.com>
2021-03-29 22:36:03 +0800

    原型模式&建造者模式占坑

---
+++ com/learn/DesignPatterns/Creation/BuilderPattern.java
@@ -0,0 +1,9 @@
+package com.learn.DesignPatterns.Creation;
+
+/**
+ * 建造者模式
+ * 场景：创建属性较为复杂的类
+ * 和工厂模式的区别：工厂模式侧重于创建不同类的对象，建造者侧重创建“一个”复杂类的对象
+ */
+public class BuilderPattern {
+}
---
+++ com/learn/DesignPatterns/Creation/PrototypePattern.java
@@ -0,0 +1,11 @@
+package com.learn.DesignPatterns.Creation;
+
+/**
+ * 原型模式：
+ * 当数值型对象占用内存空间，或者IO时延较大时，创建对象采用从现有对象中Copy的方式
+ * 主要包含：深拷贝 & 浅拷贝
+ * 深拷贝不仅复制了“指针”对象，也拷贝数值对象。浅拷贝不拷贝数值对象，和原有对象共用一个数值
+ * 场景：HashMap的深拷贝 & 浅拷贝
+ */
+public class PrototypePattern {
+}
