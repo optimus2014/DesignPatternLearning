0c5bdf9189ca88ece6adb76901e04b25b3e4e984
Yanru <forever.yjr@163.com>
2021-04-14 22:20:00 +0800

    添加模板方法模式&策略模式的Demo代码

---
+++ com/learn/DesignPatterns/Behavioral/StrategyPattern.java
@@ -0,0 +1,61 @@
+package com.learn.DesignPatterns.Behavioral;
+
+/**
+ * 策略模式
+ * 相同行为，有多种不同的策略。
+ * 角色：
+ * 1.策略接口：定义策略行为规范
+ * 2.具体策略实现类
+ * 3.策略“窗口”：用于提供给客户端，策略的使用方法
+ */
+public class StrategyPattern {
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setStrategy(new ConcreteStrategyA());
+        context.run();
+
+        context.setStrategy(new ConcreteStrategyB());
+        context.run();
+
+    }
+}
+
+// 定义策略规范
+interface Strategy {
+    void run();
+}
+
+
+
+
+// 策略管理窗口类
+class Context{
+
+    // 当前策略
+    private Strategy strategy;
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void run() {
+        this.strategy.run();
+    }
+}
+
+// 具体策略A
+class ConcreteStrategyA implements Strategy{
+    public void run() {
+        System.out.println("这是策略A的方法");
+    }
+}
+
+// 具体策略B
+class ConcreteStrategyB implements Strategy{
+    public void run() {
+        System.out.println("这是策略B的方法");
+    }
+}
--- com/learn/DesignPatterns/Behavioral/TemplateMethodPattern.java
+++ com/learn/DesignPatterns/Behavioral/TemplateMethodPattern.java
@@ -3,6 +3,45 @@ package com.learn.DesignPatterns.Behavioral;
 /**
  * 模板方法模式
  * 属于行为型模式之一。
+ * 定义一个操作中的算法骨架，而将算法的一些步骤延迟到子类中，使得子类可以不改变该算法结构的情况下重定义该算法的某些特定步骤。它是一种类行为型模式。
+ * 设计一种“按部就班” 的流程
  */
 public class TemplateMethodPattern {
+    public static void main(String[] args) {
+        TemplateClass ob1 = new ConcreteClass1();
+        TemplateClass ob2 = new ConcreteClass2();
+        ob1.run();
+        ob2.run();
+    }
+}
+
+// 模板方法
+abstract class TemplateClass{
+    // 模板方法：用于串流程
+    public void run(){
+        this.specificMethod();
+        this.abstractMethod();
+    }
+
+    private void specificMethod(){
+        System.out.println("这是公共方法");
+    }
+    // 定义轴向方法
+    protected abstract void abstractMethod();
+}
+
+// 特殊定义的对象
+class ConcreteClass1 extends TemplateClass{
+
+    protected void abstractMethod() {
+        System.out.println("这是类型1的自有方法");
+    }
+}
+
+// 特殊定义的对象
+class ConcreteClass2 extends TemplateClass{
+
+    protected void abstractMethod() {
+        System.out.println("这是类型2的自有方法");
+    }
 }
